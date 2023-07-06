507168bb144a9151dad88db434bd5479933786b1
Tzatziou, Vasileios <Vasileios.Tzatziou@umlaut.com>
2023-07-06 12:10:52 +0200

    feat: include ci files or not based on user option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import subprocess
+import sys
 
 path_conda_executable = pathlib.Path(os.environ["CONDA_EXE"])
 path_mamba_executable = path_conda_executable.parent.joinpath(
@@ -15,6 +16,28 @@ subprocess.run(
 )
 subprocess.run(["git", "branch", "-m", "main"], check=True)
 subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url}}"], check=True)
+
+# initialize ci based on user preference
+CI_FILES = ['.gitlab-ci.yml', '.gitlab-ci-test.yaml', '.gitlab-ci-stages.yaml']
+# Include CI 
+if '{{cookiecutter.include_ci_files}}'.lower() == 'yes':
+    # Select platform for the project
+    ci_platform = input('Which CI platform do you want to use? [GitLab, GitHub] ').strip().lower()
+    # GitHub
+    if ci_platform == 'github':
+        for file_name in CI_FILES:
+            file_path = os.path.join(os.getcwd(), file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        print("Sorry, GitHub CI is not supported yet. It will become available in a future version.", file=sys.stderr)
+        sys.exit(1)
+elif '{{cookiecutter.include_ci_files}}'.lower() == 'no':
+    for file_name in CI_FILES:
+        file_path = os.path.join(os.getcwd(), file_name)
+        print(os.path)
+        print(file_path)
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
 # setup environment
 subprocess.run(
